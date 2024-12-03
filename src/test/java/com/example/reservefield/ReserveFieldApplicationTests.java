0f8ea6da251ebb015a5b47578183b88ffa0ec351
Juneunbae <dmsqo1403@naver.com>
2024-12-03 10:42:41 +0900

    프로젝트 세팅

---
+++ src/test/java/com/example/reservefield/ReserveFieldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reservefield;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReserveFieldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
