05e68b61de2c1f52e012960e6d572bd106638fcc
Juneunbae <dmsqo1403@naver.com>
2024-12-11 21:57:57 +0900

    feat: setting 추가
    1. WebConfig
    - interceptor 설정 추가
    
    2. RequestInterceptor
    - preHandle, afterCompletion 설정 추가
    
    3. CustomExceptionHandler
    - AllExceptionHandler, handleHttpMessageNotReadableException 추가

--- src/main/java/com/example/reservefield/exception/CustomExceptionHandler.java
+++ src/main/java/com/example/reservefield/exception/CustomExceptionHandler.java
@@ -3,7 +3,9 @@ package com.example.reservefield.exception;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,6 +16,17 @@ import java.util.Map;
 @AllArgsConstructor
 @RestControllerAdvice
 public class CustomExceptionHandler {
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<Map<String, String>> AllExceptionHandler(CustomException e) {
+        HttpHeaders headers = new HttpHeaders();
+
+        Map<String, String> map = new HashMap<>();
+        map.put("error code", Integer.toString(e.getHttpStatus().value()));
+        map.put("error message", e.getMessage());
+
+        log.error("에러 : {}", e.getMessage());
+        return new ResponseEntity<>(map, headers, e.getHttpStatus());
+    }
 
     @ExceptionHandler(value = CustomException.class)
     public ResponseEntity<Map<String, String>> ExceptionHandler(CustomException e) {
@@ -25,5 +38,18 @@ public class CustomExceptionHandler {
 
         log.error(e.getMessage());
         return new ResponseEntity<>(map, headers, e.getHttpStatus());
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Map<String, String>> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        log.error("Invalid json", e);
+        HttpHeaders headers = new HttpHeaders();
+
+        Map<String, String> map = new HashMap<>();
+        map.put("error code", String.valueOf(HttpStatus.BAD_REQUEST.value()));
+        map.put("error message", e.getMessage());
+
+        log.error(e.getMessage());
+        return new ResponseEntity<>(map, headers, HttpStatus.BAD_REQUEST);
     }
 }
